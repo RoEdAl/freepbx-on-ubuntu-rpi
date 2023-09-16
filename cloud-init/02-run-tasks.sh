35593c7c92c6a1594d77f155afe94213d2320329
Edmunt Pienkowsky <roed@onet.eu>
2023-09-16 15:31:36 +0200

    Internal task improvements

--- cloud-init/02-run-tasks.sh
+++ cloud-init/02-run-tasks.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 TASK_DIR=/usr/src/ubuntu-freepbx/tasks
 tar -xf ${TASK_DIR}.tar -C /usr/src/ubuntu-freepbx
 rm ${TASK_DIR}.tar
-for t in systemd-journal modprobe systemd-units rm-pkgs pam freepbx rpi; do
-    task -d ${TASK_DIR}/${t}
-done
+task -d ${TASK_DIR}
